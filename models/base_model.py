dda8aacbb16e9f6e49c434acac018d88305ba15c
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-03-22 09:23:52 +0100

    Task 06: still in development

--- models/base_model.py
+++ models/base_model.py
@@ -14,17 +14,16 @@ class BaseModel:
     id = Column(String(60), nullable=False, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            for at in ['created_at', 'updated_at']:
+                kwargs[at] = datetime.fromisoformat(kwargs[at])
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
@@ -48,10 +47,14 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        try:
+            del dictionary['_sa_instance_state']
+        except KeyError:
+            pass
         return dictionary
 
 
     def delete(self):
         """Delete the class instance"""
         from models import storage
-        del storage.all()[self.to_dict()['__class__'] + '.' + self.id]
+        storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete', backref='state')
+
+    @property
+    def cities(self):
+        """A getter for the attribute cities"""
+        return [city for city in self.cities if city.state_id == self.id]
