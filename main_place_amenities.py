77cd613fac86036b7f07c9cd3321dfbddb1e5643
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-06 10:16:03 +0100

    10

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -7,6 +7,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models import storage
 
 # creation of a State
 state = State(name="California")
--- models/base_model.py
+++ models/base_model.py
@@ -21,14 +21,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+        if kwargs:
             for at in ['created_at', 'updated_at']:
-                kwargs[at] = datetime.fromisoformat(kwargs[at])
-            del kwargs['__class__']
+                if at in kwargs.keys():
+                    kwargs[at] = datetime.fromisoformat(kwargs[at])
+            if '__class__' in kwargs.keys():
+                del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
