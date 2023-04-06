213dfc0e438023d0f13f0daf8aa698d8e09eba4b
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-06 16:21:59 +0100

    01

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,10 @@ from datetime import datetime
 def do_pack():
     """Compress the contents of web_static"""
 
+    #  create `versions` dir if not exists
     local('mkdir -p versions')
+
+    #  create compressed tgz file
     time_stamp = datetime.now().strftime('%Y%m%d%H%M%S')
     path = 'versions/web_static_' + time_stamp + '.tgz'
     result = local('tar -cvzf {} web_static/'.format(path))
