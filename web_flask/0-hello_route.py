f31119dd8e2eea82687f5d08189973537ac56765
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-21 12:25:38 +0100

    Task 00

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A simple web application using flask"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """a simple index page for my web application"""
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
