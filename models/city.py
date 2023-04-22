c02c132df51479dc5936401927ef964447ed3bae
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-22 22:08:44 +0100

    Task 07

--- models/city.py
+++ models/city.py
@@ -8,8 +8,15 @@ import os
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'cities'
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'),
+                          nullable=False)
 
-    places = relationship('Place', cascade='all, delete', backref='cities')
+        places = relationship('Place', cascade='all, delete',
+                              backref='cities')
+
+    else:
+        name = ""
+        state_id = ""
