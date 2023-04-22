cd20e1170c1c66934a610f29d41495f0c6493f99
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-22 21:39:44 +0100

    Task 077

--- models/state.py
+++ models/state.py
@@ -9,9 +9,13 @@ import os
 class State(BaseModel, Base):
     """ State class """
 
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete', backref='state')
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        name = ""
+        cities = []
 
     @property
     def cities(self):
