70e71ebfdc5dad7db308d8367b6dcc7fca485891
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-04-23 22:14:03 +0100

    Task 07

--- models/amenity.py
+++ models/amenity.py
@@ -5,9 +5,6 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
 
-if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
-    from models.place import place_amenity
-
 
 class Amenity(BaseModel, Base):
     """ Amenties Class """
@@ -15,8 +12,5 @@ class Amenity(BaseModel, Base):
         __tablename__ = 'amenities'
 
         name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place",
-                                       secondary=place_amenity,
-                                       backref="amenities")
     else:
         name = ""
